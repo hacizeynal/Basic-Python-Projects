8d22b61cd08d2b9d92938af76b22c0441a470a07
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-07-19 21:55:50 +0200

    v1.0 twitter question least common name

--- twitter_find_least_common_name.py
+++ twitter_find_least_common_name.py
@@ -1,4 +1,11 @@
-a = ["John","Zeynal","John","Faraj","John"]
+# Find the least common name used in below list name a
+from collections import Counter
+cnt = Counter()
+
+
+a = ["John","Zeynal","John","Faraj","Zeynal","John"]
 
 for i in a:
-    if
+    cnt[i] = cnt[i] + 1
+    print(cnt[i])
+print("Most common used name is {}".format(cnt.most_common(1)[0][0]))
